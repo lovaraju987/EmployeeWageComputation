89d49c161378a16b2cf3c5d29e73a7a562f3a61b
Lovaraju Mailapalli <mailapallilovaraaju987@gmail.com>
2021-04-14 09:26:49 +0530

    checking employee is present or not

--- Program.cs
+++ Program.cs
@@ -7,6 +7,22 @@ namespace EmployeeWageComputation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
+
+            int present = 1;
+
+            Random random = new Random();
+            int employeeCheck = random.Next(0, 2);
+
+            Console.WriteLine("random value" + employeeCheck);
+            if (employeeCheck == present)
+            {
+                Console.WriteLine("Employee is Present.");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent.");
+            }
+            Console.ReadLine();
         }
     }
 }
